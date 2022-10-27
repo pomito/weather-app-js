9bae562bfa3ebf987ae533a645aa0165f2d0831c
Sebastian Pomiklo <spomiklo@gmail.com>
2022-10-27 15:00:20 +0200

    updating the UI, weather icons & images

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,2 +1,32 @@
 const key = 'fXsXdyC0gJA1zUiqSWYGF8pXuBuyOnp1';
 
+//get weather information
+
+const getWeather =  async (id) =>{
+
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+
+//get city information
+const getCity = async (city) => {
+
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+
+};
+
+
+
+
